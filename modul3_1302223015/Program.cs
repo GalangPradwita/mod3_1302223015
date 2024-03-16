f77b31f56afe0706cf5fd67c64c19548a2209e72
GalangPradwita <mgalangpradwita16@gmail.com>
2024-03-16 08:05:22 +0700

    first commit

---
+++ modul3_1302223015/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace modul3_1302223015
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
